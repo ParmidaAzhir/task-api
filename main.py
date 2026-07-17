9fe441a2e0dfdbb13438db057c028b3da1b55d94
Parmida Azhir <parmidaazhir@gmail.com>
2026-07-17 22:50:15 +0200

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/") #If someone sends a GET request to the path /, execute the function below.
 def root():
-    return {"message": "Hello World"}
+    return {
+    "name": "Task API",
+    "version": "1.0",
+    "endpoints": ["/tasks"]
+}
+@app.get("/health") #When someone requests /health, FastAPI runs the health() function and returns the status.
+def health():
+    return {"status": "ok"}
