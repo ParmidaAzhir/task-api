695bf46c8edc6c89d7e2bc7914a63783731ff655
Parmida Azhir <parmidaazhir@gmail.com>
2026-08-05 10:57:19 +0200

    Stage 1: connect via .env and create table

--- main.py
+++ main.py
@@ -1,42 +1,12 @@
 from fastapi import FastAPI, Body, status
 from fastapi.responses import JSONResponse
 import sqlite3
+from repository import initialize_database
 
-conn = sqlite3.connect("tasks.db")
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    done BOOLEAN NOT NULL DEFAULT 0
-)
-""")
-
-# Check how many tasks already exist
-cursor.execute("SELECT COUNT(*) FROM tasks")
-count = cursor.fetchone()[0]
-
-# Insert the example tasks only if the table is empty
-if count == 0:
-    cursor.executemany("""
-        INSERT INTO tasks (title, done)
-        VALUES (?, ?)
-    """, [
-        ("Study FastAPI", False),
-        ("Buy groceries", True),
-        ("Go to the gym", False)
-    ])
-
-conn.commit()
-
+initialize_database()
 
 app = FastAPI()
-tasks = [
-    {"id": 1, "title": "Study FastAPI", "done": False},
-    {"id": 2, "title": "Buy groceries", "done": True},
-    {"id": 3, "title": "Go to the gym", "done": False},
-]
+
 @app.get("/", summary="Get API information") #If someone sends a GET(get reads data) request to the path /, execute the function below. 
 def root():
     return {
@@ -225,7 +195,7 @@ def delete_task(id: int):
 
     db.commit()
     db.close()
-    
+
 @app.get("/stats", summary="Get task statistics")
 def get_stats():
     total = len(tasks)
